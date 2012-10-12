345df1146cbf3b8ff9b894588657ba461732636f
zKalev <zhivko_kalev@abv.bg>
2012-10-12 15:47:04 +0300

    added new ParserException

---
+++ UserAgentParser/src/ParseExeption.java
@@ -0,0 +1,17 @@
+package ua.paket;
+
+public class ParseExeption extends Exception {
+	 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParseExeption(){
+		System.out.println("ParseExeption");
+		
+		
+		
+	}
+
+}
