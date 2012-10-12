e285a773b114091fe9d00e32c4f6f40d687c46c0
mparshorov <martin.parshorov@gmail.com>
2012-10-12 13:26:53 +0300

    zemi

--- UserAgentParser/src/UserAgentParser.java
+++ UserAgentParser/src/UserAgentParser.java
@@ -5,6 +5,8 @@ public class UserAgentParser {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String regexForBlackBerry4and5 = "(blackberry)+?\\d{2,4}?.*?/(\\d+.\\d+.\\d*?.\\d*?)+?((profile/midp-)+?" +
+				"(\\d.\\d)+?)*?((configuration/)+?(\\w+?-\\d.\\d)+?)*?((vendorid/)+?\\d+?)*?.*?";
 		// TODO Auto-generated method stub
 
 	}
