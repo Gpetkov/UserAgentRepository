ce2b076fae0c24b1b8cdf91c1b9a3b4e6f9386f3
zKalev <zhivko_kalev@abv.bg>
2012-10-12 13:55:18 +0300

    Jivko changes

--- UserAgentParser/src/UserAgent.java
+++ UserAgentParser/src/UserAgent.java
@@ -1,12 +1,78 @@
 
+
+
 public class UserAgent {
+enum type {
+			SMARTPHON, TABLET, PS
+		}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private String hardware;
+		private String model;
+		private String modelVersion;
+		private String software;
+		private String softwareVersion;
 
-	}
+		public UserAgent(String mHardware, String mModel,
+				String mModelVersion, String mSoftware, String mSoftwareVersion) {
+			setHardware(mHardware);
+			setModel(mModel);
+			setModelVersion(mModel);
+			setSoftware(mSoftware);
+			setSoftwareVersion(mSoftwareVersion);
+
+		}
+
+		public UserAgent() {
+			this(null, null, null, null, null);
+
+		}
+
+		public UserAgent(UserAgent uap) {
+			this(uap.getHardware(), uap.getModel(), uap.getModelVersion(), uap
+					.getSoftware(), uap.getSoftwareVersion());
+
+		}
+
+		public String getHardware() {
+			return hardware;
+		}
+
+		public void setHardware(String hardware) {
+			this.hardware = hardware;
+		}
+
+		public String getModel() {
+			return model;
+		}
+
+		public void setModel(String model) {
+			this.model = model;
+		}
+
+		public String getModelVersion() {
+			return modelVersion;
+		}
+
+		public void setModelVersion(String modelVersion) {
+			this.modelVersion = modelVersion;
+		}
+
+		public String getSoftware() {
+			return software;
+		}
+
+		public void setSoftware(String software) {
+			this.software = software;
+		}
+
+		public String getSoftwareVersion() {
+			return softwareVersion;
+		}
+
+		public void setSoftwareVersion(String softwareVersion) {
+			this.softwareVersion = softwareVersion;
+		}
+
+
 
 }
