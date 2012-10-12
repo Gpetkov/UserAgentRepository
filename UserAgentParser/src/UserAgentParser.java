04efec135d9d89122d41778b5cb5f115581c30da
NGizdov <nedelin_g_g@abv.bg>
2012-10-12 15:02:14 +0300

    added userAgentParser methods

--- UserAgentParser/src/UserAgentParser.java
+++ UserAgentParser/src/UserAgentParser.java
@@ -1,14 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class UserAgentParser {
+	
+	private File file;	
+	public ArrayList<UserAgent> userAgents;
+	public String regex = "(blackberry)+?(\\d{2,4}?.*)*?/(\\d+.\\d+.\\d*?.\\d*?)+?((profile/midp-)+?" +
+			"(\\d.\\d)+?)*?((configuration/)+?(\\w+?-\\d.\\d)+?)*?((vendorid/)+?\\d+?)*?.*?";
+	
+	public File getFile() {
+		return file;
+	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String regexForBlackBerry4and5 = "(blackberry)+?\\d{2,4}?.*?/(\\d+.\\d+.\\d*?.\\d*?)+?((profile/midp-)+?" +
-				"(\\d.\\d)+?)*?((configuration/)+?(\\w+?-\\d.\\d)+?)*?((vendorid/)+?\\d+?)*?.*?";
-		// TODO Auto-generated method stub
+	public void setFile(File file) {
+		this.file = file;
+	}
 
+	public UserAgentParser(File file){
+		this.file = file;
+	}
+	
+	public UserAgentParser(){
+		this(null);
+	}
+	
+	public void readFileLines(){
+		Scanner input = null;
+		try {
+			input = new Scanner(getFile());
+			Pattern p = Pattern.compile(regex);
+			
+			while(input.hasNextLine()){
+				String line = input.nextLine();
+				Matcher m = p.matcher(line);
+				if (m.find()){
+					String userAgent = m.toString();
+					parse(userAgent);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public UserAgent parse(String line){
+		return null;
 	}
 
 }
