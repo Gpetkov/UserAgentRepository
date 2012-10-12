fbd886dc6c0b3cd7bbf4068d68f5202d3476fb66
zKalev <zhivko_kalev@abv.bg>
2012-10-12 16:48:30 +0300

    added new propert

--- UserAgentParser/src/UserAgent.java
+++ UserAgentParser/src/UserAgent.java
@@ -1,78 +1,95 @@
-
-
-
+/**
+ * 
+ * @author BitMix
+ * This class contain info about user agent request
+ *
+ */
 public class UserAgent {
-enum type {
-			SMARTPHON, TABLET, PS
+	// type of device
+	enum deviceType {
+		SMARTPHON, TABLET, PC
+		
+	}
+	
+	//user agent type 
+	 UserAgent type;
+	// hardware model for example BlackBerry
+	private String hardware;
+	//device's model for example Bold
+	private String model;
+	// version's model for example 9900
+	private String modelVersion;
+	//software platform for example java
+	private String software;
+	// software's version for example 5.0
+	private String softwareVersion;
+
+	public UserAgent(String mHardware, String mModel, String mModelVersion,
+			String mSoftware, String mSoftwareVersion,UserAgent mType) {
+		setHardware(mHardware);
+		setModel(mModel);
+		setModelVersion(mModel);
+		setSoftware(mSoftware);
+		setSoftwareVersion(mSoftwareVersion);
+		setType(mType);
+
+	}
+
+	public UserAgent() {
+		this(null, null, null, null, null,null);
+
+	}
+
+	public UserAgent(UserAgent uap) {
+		this(uap.getHardware(), uap.getModel(), uap.getModelVersion(), uap
+				.getSoftware(), uap.getSoftwareVersion(),uap.getType());
+
+	}
+
+	public String getHardware() {
+		return hardware;
+	}
+
+	public void setHardware(String hardware) {
+		this.hardware = hardware;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getModelVersion() {
+		return modelVersion;
+	}
+
+	public void setModelVersion(String modelVersion) {
+		this.modelVersion = modelVersion;
+	}
+
+	public String getSoftware() {
+		return software;
+	}
+
+	public void setSoftware(String software) {
+		this.software = software;
+	}
+
+	public String getSoftwareVersion() {
+		return softwareVersion;
+	}
+
+	public void setSoftwareVersion(String softwareVersion) {
+		this.softwareVersion = softwareVersion;
+	}
+	  public UserAgent getType() {
+			return type;
 		}
 
-		private String hardware;
-		private String model;
-		private String modelVersion;
-		private String software;
-		private String softwareVersion;
-
-		public UserAgent(String mHardware, String mModel,
-				String mModelVersion, String mSoftware, String mSoftwareVersion) {
-			setHardware(mHardware);
-			setModel(mModel);
-			setModelVersion(mModel);
-			setSoftware(mSoftware);
-			setSoftwareVersion(mSoftwareVersion);
-
+		public void setType(UserAgent type) {
+			this.type = type;
 		}
-
-		public UserAgent() {
-			this(null, null, null, null, null);
-
-		}
-
-		public UserAgent(UserAgent uap) {
-			this(uap.getHardware(), uap.getModel(), uap.getModelVersion(), uap
-					.getSoftware(), uap.getSoftwareVersion());
-
-		}
-
-		public String getHardware() {
-			return hardware;
-		}
-
-		public void setHardware(String hardware) {
-			this.hardware = hardware;
-		}
-
-		public String getModel() {
-			return model;
-		}
-
-		public void setModel(String model) {
-			this.model = model;
-		}
-
-		public String getModelVersion() {
-			return modelVersion;
-		}
-
-		public void setModelVersion(String modelVersion) {
-			this.modelVersion = modelVersion;
-		}
-
-		public String getSoftware() {
-			return software;
-		}
-
-		public void setSoftware(String software) {
-			this.software = software;
-		}
-
-		public String getSoftwareVersion() {
-			return softwareVersion;
-		}
-
-		public void setSoftwareVersion(String softwareVersion) {
-			this.softwareVersion = softwareVersion;
-		}
-
-
-
 }
