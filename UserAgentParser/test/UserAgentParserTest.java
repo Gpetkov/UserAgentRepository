a6cfa196a2cbdf3685971c1993b740e4f674b9e9
Georgi <petkov_hcb@abv.bg>
2012-10-17 12:07:31 +0300

    Added new tests for the "parse" method

--- UserAgentParser/test/UserAgentParserTest.java
+++ UserAgentParser/test/UserAgentParserTest.java
@@ -1,38 +1,75 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class UserAgentParserTest {
-
-	UserAgentParser testUserAgentParser = new UserAgentParser() {
-	};
-	ParseExeption testParseException = new ParseExeption();
-	String input = "BlackBerry8520/4.6.1.314 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/301,platform,unknown";
-	String input1 = "BlackBerry9300/6.6.0.124 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/310 The Times/1.0,platform,unknown";
-	String input2 = "BlackBerry9300/6.6.0.124 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/609 The Times/1.0 UP.Link/6.5.1.3.0,platform,unknown";
-	UserAgent result = new UserAgent("BlackBerry", "BlackBerry8520", "8520", "OS4", "4.6.1.314", UserAgent.DeviceType.SMARTPHONE);
+	//private static final String input = "BlackBerry8520/4.6.1.314 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/301,platform,unknown";
+	//private static final String input1 = "BlackBerry9300/6.6.0.124 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/310 The Times/1.0,platform,unknown";
+	//private static final String input2 = "BlackBerry9300/6.6.0.124 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/609 The Times/1.0 UP.Link/6.5.1.3.0,platform,unknown";
+	
+	private static String userAgentString;
+	private static ParseExeption parseException;
+	private static UserAgent expectedUserAgent;
+	//private static UserAgent actualUserAgent;
+	private static UserAgentParser testUserAgentParser;
+	@BeforeClass
+	public static void setUpBeforeClass()throws Exception{
+		parseException = new ParseExeption();
+		//expectedUserAgent = new UserAgent("BlackBerry", "BlackBerry8520", "8520", "OS4", "4.6.1.314", UserAgent.DeviceType.SMARTPHONE);
+		testUserAgentParser = new UserAgentParser() {};
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception{
+		parseException = null;
+		expectedUserAgent = null;
+		testUserAgentParser = null;
+		userAgentString = null;
+		//actualUserAgent = null;
+	}
+	
+	public UserAgentParserTest(String currentUserAgentString, UserAgent currentExpectedUserAgent, ParseExeption currentException){
+		userAgentString = currentUserAgentString;
+		expectedUserAgent = currentExpectedUserAgent;
+		parseException = currentException;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Parameters
+	public static Collection parserTestExValues(){
+		Object[][] exeValues = {{"BlackBerry8520/4.6.1.314 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/301,platform,unknown",
+			new UserAgent("BlackBerry", "BlackBerry8520", "8520", "OS4", "4.6.1.314", UserAgent.DeviceType.SMARTPHONE),
+			null},
+			{"BlackBerry9300/6.6.0.124 Profile/MIDP-2.0 Configuration/CLDC-1.1 VendorID/310 The Times/1.0,platform,unknown",
+				new UserAgent("BlackBerry", "BlackBerry9300", "9300", "OS6", "6.6.0.124", UserAgent.DeviceType.SMARTPHONE),
+				null},
+				{"BlackBerry8520/5.0.0.1022 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/115,platform,unknown",
+					new UserAgent("BlackBerry", "BlackBerry8520", "8520", "OS4", "4.6.1.314", UserAgent.DeviceType.SMARTPHONE),
+					null}};
+		return Arrays.asList(exeValues);
+	}
+	 
+	//ParseExeption testParseException = new ParseExeption();
+	
+	//UserAgent result = new UserAgent("BlackBerry", "BlackBerry8520", "8520", "OS4", "4.6.1.314", UserAgent.DeviceType.SMARTPHONE);
 
 	@Test
 	public void testParse() {
 		// assertEquals("Result ", (Object)result,(Object)
 		// testUserAgentParser.parse(input));
 	//	assertEquals("Test with null string", result, testUserAgentParser.parse(null));
-		assertEquals("Test 1", result, testUserAgentParser.parse(input));
+		assertEquals("The regular expression didn't find the same user agent!", expectedUserAgent, testUserAgentParser.parse(userAgentString));
 	//	assertEquals("Test2", null, testUserAgentParser.parse(input1));
 	//	assertEquals("", testParseException, testUserAgentParser.parse(input2));
 
 	}
-	/*
-	 * @Test public void testParse1() { assertEquals("", null,
-	 * testUserAgentParser.parse(input2)); }
-	 * 
-	 * @Test public void testParse2() { assertEquals("", result,
-	 * testUserAgentParser.parse(null)); }
-	 * 
-	 * @Test public void testParse3() { assertEquals("", result,
-	 * testUserAgentParser.parse(input1)); }
-	 * 
-	 * @Test public void testParse4() { assertEquals("", result,
-	 * testUserAgentParser.parse(input)); }
-	 */
+	
 }
