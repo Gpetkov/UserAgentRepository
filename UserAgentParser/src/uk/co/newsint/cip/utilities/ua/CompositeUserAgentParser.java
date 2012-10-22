58bd31f4e4d6da313625b6a9f71f4aaf6c455c8d
zKalev <zhivko_kalev@abv.bg>
2012-10-22 16:14:39 +0300

    New get for the browser name

--- UserAgentParser/src/uk/co/newsint/cip/utilities/ua/CompositeUserAgentParser.java
+++ UserAgentParser/src/uk/co/newsint/cip/utilities/ua/CompositeUserAgentParser.java
@@ -7,7 +7,7 @@ package uk.co.newsint.cip.utilities.ua;
  * @author Zhivko Kalev
  * @since 1.0
  */
-<<<<<<< HEAD
+
 public class CompositeUserAgentParser extends UserAgentParser {
 
 	protected UserAgentUtilsParser utilsParser = new UserAgentUtilsParser();
@@ -45,46 +45,5 @@ public class CompositeUserAgentParser extends UserAgentParser {
 
 		return userAgent;
 	}
-=======
-public class CompositeUserAgentParser extends UserAgentParser
-{
-
-    protected UserAgentUtilsParser utilsParser = new UserAgentUtilsParser();
-    protected RegexpUserAgentParser regexpParser = new RegexpUserAgentParser();
-
-    @Override
-    public UserAgent parse(String userAgentString)
-    {
-        UserAgent userAgent = null;
-
-        // 1. Use UserAgentUtilsParser to try parse the UA string
-
-        userAgent = utilsParser.parse(userAgentString);
-
-        // 2. Use UserAgentParser to try parse the UA string
-        UserAgent regexpAgent = regexpParser.parse(userAgentString);
-
-        // 3. Fill Unkowns with regexp UA properties
-        // TODO (Zhivko): Consider using equals, not ==
-        if (UserAgent.UNKNOWN.equals(userAgent.getDeviceType()))
-            userAgent.setDeviceType(regexpAgent.getDeviceType());
-        if (UserAgent.UNKNOWN.equals(userAgent.getDeviceMaker()))
-            userAgent.setDeviceMaker(regexpAgent.getDeviceMaker());
-        if (UserAgent.UNKNOWN.equals(userAgent.getDeviceModel()))
-            userAgent.setDeviceModel(regexpAgent.getDeviceModel());
-        if (UserAgent.UNKNOWN.equals(userAgent.getDeviceModelVersion()))
-            userAgent.setDeviceModelVersion(regexpAgent.getDeviceModelVersion());
-        if (UserAgent.UNKNOWN.equals(userAgent.getOS()))
-            userAgent.setOS(regexpAgent.getOS());
-        if (UserAgent.UNKNOWN.equals(userAgent.getOSVersion()))
-            userAgent.setOSVersion(regexpAgent.getOSVersion());
-        if (UserAgent.UNKNOWN.equals(userAgent.getBrowser()))
-            userAgent.setBrowser(regexpAgent.getBrowser());
-        if (UserAgent.UNKNOWN.equals(userAgent.getBrowserVersion()))
-            userAgent.setBrowserVersion(regexpAgent.getBrowserVersion());
-
-        return userAgent;
-    }
->>>>>>> 318661e5dc5e7978aee576c35fa637954698c296
 
 }
--- UserAgentParser/src/uk/co/newsint/cip/utilities/ua/UserAgentUtilsParser.java
+++ UserAgentParser/src/uk/co/newsint/cip/utilities/ua/UserAgentUtilsParser.java
@@ -11,7 +11,7 @@ import nl.bitwalker.useragentutils.Version;
  * @author Zhivko Kalev
  * @since 1.0
  */
-<<<<<<< HEAD
+
 public class UserAgentUtilsParser extends UserAgentParser {
 
 	@Override
@@ -47,52 +47,6 @@ public class UserAgentUtilsParser extends UserAgentParser {
 
 		return result;
 	}
-=======
-public class UserAgentUtilsParser extends UserAgentParser
-{
 
-    @Override
-    public UserAgent parse(String userAgentString)
-    {
-        // ask the library to parse the UA
-        nl.bitwalker.useragentutils.UserAgent ua = nl.bitwalker.useragentutils.UserAgent.parseUserAgentString(userAgentString);
-
-        // TODO (Zhivko): check when ua is unparsed, throw exception
-
-        // construct UserAgent result
-        UserAgent result = new UserAgent();
-        OperatingSystem os = ua.getOperatingSystem();
-        if (os != null)
-        {
-            DeviceType deviceType = os.getDeviceType();
-            if (deviceType != null)
-            {
-                result.setDeviceType(deviceType.getName());
-
-                // TODO (Zhivko): Add device model, model version and maker
-            }
-
-            result.setOS(os.getGroup().getName());
-            // this include operating system and operating system's Version for
-            // example Windows_XP
-            // if (os.getGroup() == os)
-            // result.setOSVersion(os.getName());
-        }
-
-        Browser browser = ua.getBrowser();
-        if (browser != null)
-        {
-            result.setBrowser(browser.getName());
-        }
-
-        Version version = ua.getBrowserVersion();
-        if (version != null)
-        {
-            result.setBrowserVersion(version.getVersion());
-        }
-
-        return result;
-    }
->>>>>>> 318661e5dc5e7978aee576c35fa637954698c296
 
 }
