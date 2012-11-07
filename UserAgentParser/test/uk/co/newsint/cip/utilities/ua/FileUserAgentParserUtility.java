61cdbe943e2b4a247524ad4e197bcbc16966b566
U-BXWin101\BitMix <BitMix@BXWin101.(none)>
2012-11-07 18:19:50 +0200

    Added new tests for eV  Please enter the commit message for your changes. Lines starting

--- UserAgentParser/test/uk/co/newsint/cip/utilities/ua/FileUserAgentParserUtility.java
+++ UserAgentParser/test/uk/co/newsint/cip/utilities/ua/FileUserAgentParserUtility.java
@@ -74,7 +74,7 @@ public class FileUserAgentParserUtility extends FileUserAgentParser
      * This method create a CSV file and adding in it header fields
      * 
      */
-    private void createFile()
+    protected void createFile()
     {
         try
         {
@@ -125,7 +125,7 @@ public class FileUserAgentParserUtility extends FileUserAgentParser
      * 
      * @param lineNumber,userAgentString,currentUserAgent
      */
-    private void generateCsvFileContent(int lineNumber, String userAgentString, UserAgent currentUserAgent)
+    protected void generateCsvFileContent(int lineNumber, String userAgentString, UserAgent currentUserAgent)
     {
         // Counter for properties with Unknown value
         int counterUnknown = 0;
